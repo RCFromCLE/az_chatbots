9f21cf2b4ace869ddff2768b28bf7cca8a5f6d99
Rudy <rudycorradetti4@gmail.com>
2024-07-07 03:06:34 -0400

    updated tf

--- bot_infra/main.tf
+++ bot_infra/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.39.0"
+      version = "~> 2.53.0"
     }
   }
 }
