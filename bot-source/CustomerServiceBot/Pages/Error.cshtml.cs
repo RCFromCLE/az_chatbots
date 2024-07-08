c7bec495f2dc285215678688ac74ad5e6f3e7163
Rudy <rudycorradetti4@gmail.com>
2024-07-07 23:22:13 -0400

    added botcontroller.cs and updated program.cs, added front end for chat

--- bot-source/CustomerServiceBot/Pages/Error.cshtml.cs
+++ bot-source/CustomerServiceBot/Pages/Error.cshtml.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CustomerServiceBot.Pages;
 
